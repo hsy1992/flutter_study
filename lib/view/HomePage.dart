d394fae2494c15d6512d1fff20f622e12fd36b38
hsy1992 <553012786@qq.com>
2019-06-18 09:30:12 +0800

    banner

--- lib/view/HomePage.dart
+++ lib/view/HomePage.dart
@@ -5,6 +5,7 @@ import 'package:flutter_study/view/BasePage.dart';
 import 'package:flutter_study/http/Api.dart';
 import 'package:flutter_study/constant/Constant.dart';
 import 'package:flutter_study/model/BannerBean.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends BasePage {
 
@@ -22,6 +23,8 @@ class HomePageView extends StatefulWidget {
 
 class HomePageViewState extends State<HomePageView> {
 
+  List<BannerBean> list = [];
+
   @override
   void initState() {
     super.initState();
@@ -35,16 +38,47 @@ class HomePageViewState extends State<HomePageView> {
     return Container(
       child: ListView(
         children: <Widget>[
+          new Container(
+            width: MediaQuery.of(context).size.width,
+            height: 200.0,
+            child: Swiper(
+              itemCount: list.length == 0 ? 1 : list.length,
+              itemBuilder: _swiperBuilder,
+              scrollDirection: Axis.horizontal,
+              autoplay: true,
+              onTap: (index) => debugPrint('点击了第$index个'),
+              pagination: new SwiperPagination(
+                  builder: DotSwiperPaginationBuilder(
+                      color: Colors.black54,
+                      activeColor: Colors.white
+                  )
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 
+  //获取banner数据
   void _getBannerData() async {
     List<BannerBean> bannerBeans = await Api.getApi().getBannerData();
     if (bannerBeans != null) {
       print(bannerBeans);
+      setState(() {
+        list = bannerBeans;
+      });
     }
+  }
+
+  Widget _swiperBuilder(BuildContext context, int index) {
+
+    if (list.length == 0) {
+      return Image.network("http://via.placeholder.com/350x150", fit: BoxFit.fill,);
+    } else {
+      return Image.network(list[index].imagePath, fit: BoxFit.fill,);
+    }
+
   }
 }
 
